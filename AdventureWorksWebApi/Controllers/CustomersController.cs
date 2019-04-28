3d518ef3b63f512a1f8f4cea534869865f015fb3
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-04-28 20:48:27 +0200

    Add project files.

---
+++ AdventureWorksWebApi/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AdventureWorksWebApi.Models;
+
+namespace AdventureWorksWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly AdventureWorksLTContext _context;
+
+        public CustomersController(AdventureWorksLTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers/email
+        [HttpGet("{email}")]
+        public ActionResult<CustomerShort> GetCustomer(string email)
+        {
+            var customer = _context.Customer.FirstOrDefault(x => x.EmailAddress == email);
+
+            if (customer != null)
+            {
+                return new CustomerShort()
+                {
+                    EmailAddress = customer.EmailAddress,
+                    CompanyName = customer.CompanyName,
+                    CustomerName = $"{customer.Title} {customer.FirstName} {customer.LastName} {customer.Suffix}"
+                };
+            }
+
+            return default(CustomerShort);
+        }
+    }
+}
